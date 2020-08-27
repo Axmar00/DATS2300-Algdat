8c3cf3435be1466df584b9bf354095e037662b44
asmar <asmar-nasar2000@hotmail.com>
2020-08-27 20:38:01 +0200

    1.2.8

--- src/ukeoppgaver/Ukeoppgave2.java
+++ src/ukeoppgaver/Ukeoppgave2.java
@@ -28,6 +28,41 @@ public class Ukeoppgave2 {
         Gjort i Tabell.java
          */
 
+        /*-------1.2.6-------*/
+        /*Deloppgave2
+        Metoden vil bruke færrest mulige sammenligninger i tabeller som er
+        korte og tabeller som har maksverdien og nest-maksverdien i en av de første posisjonene.
+         */
+        /*Deloppgave3
+
+         */
+
+        /*-------1.2.8-------*/
+        /*Deloppgave1
+        Trengs 15 for 16 deltakere. 7 for 8. 2^k - 1 for 2^k.
+         */
+
+        /*Deloppgave2
+        a)
+        3, 15, 8, 11, 13, 9, 10, 5
+        15 11 13 10
+        15 13
+        15
+        b)
+        10, 17, 13, 16, 17, 8, 12, 15, 9, 15, 16, 15, 10, 13, 14, 17
+        17 16 17 15 15 16 13 17
+        17 17 16 17
+        17 17
+        17
+         */
+        /*Deloppgave3
+        9, 21, 5, 25, 27, 23, 26, 28, 17, 15, 19, 18, 16, 20, 14, 10
+        9 5 23 26 15 18 16 10
+        5 23 15 10
+        5 10
+        5
+        */
+        /*-------1.2.9-------*/
 
     }
 }
