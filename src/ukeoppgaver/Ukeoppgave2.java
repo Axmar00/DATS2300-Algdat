143a52ea964d781f3c490057283574c0e7de9803
asmar <asmar-nasar2000@hotmail.com>
2020-08-30 11:57:27 +0200

    1.2.13 Del1

--- src/ukeoppgaver/Ukeoppgave2.java
+++ src/ukeoppgaver/Ukeoppgave2.java
@@ -2,6 +2,10 @@ package ukeoppgaver;
 
 public class Ukeoppgave2 {
 
+    public static void kopier(int[] a, int i, int[] b, int j, int length){
+        for(int n = )
+
+    }
 
     public static void main(String[] args){
         /*-------1.2.1-------*/
@@ -129,7 +133,42 @@ public class Ukeoppgave2 {
         Finnes 5 forskjellige fulle binærtrær med 7 noder.
          */
 
-        
+        /*-------1.2.13-------*/
+        /*Deloppgave1
+        public static int[] nestMaks(int[] a)   // en turnering
+    {
+        int n = a.length;                // for å forenkle notasjonen
+
+        if (n < 2) // må ha minst to verdier!
+            throw new IllegalArgumentException("a.length(" + n + ") < 2!");
+
+        int[] b = new int[2*n];          // turneringstreet
+        System.arraycopy(a,0,b,n,n);     // legger a bakerst i b
+
+        for (int k = 2*n-2; k > 1; k -= 2)   // lager turneringstreet
+            b[k/2] = Math.max(b[k],b[k+1]);
+
+        for(int i = b.length-1; i > 0; --i){   // Skriver ut resultatet av turneringen
+            System.out.print(b[i] + " ");
+        }
+
+        int maksverdi = b[1], nestmaksverdi = Integer.MIN_VALUE;
+
+        for (int m = 2*n - 1, k = 2; k < m; k *= 2)
+        {
+            int tempverdi = b[k+1];  // ok hvis maksverdi er b[k]
+            if (maksverdi != b[k]) { tempverdi = b[k]; k++; }
+            if (tempverdi > nestmaksverdi) nestmaksverdi = tempverdi;
+        }
+
+        return new int[] {maksverdi,nestmaksverdi}; // størst og nest størst
+
+    }
+         */
+
+        /*Deloppgave3
+
+         */
 
 
 
