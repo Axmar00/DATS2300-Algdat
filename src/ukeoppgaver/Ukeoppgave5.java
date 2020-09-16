150d828a6126441fb97a4f0418bc517ffcd65d1a
asmar <asmar-nasar2000@hotmail.com>
2020-09-16 21:41:21 +0200

    1.5.7

--- src/ukeoppgaver/Ukeoppgave5.java
+++ src/ukeoppgaver/Ukeoppgave5.java
@@ -105,7 +105,38 @@ public class Ukeoppgave5 {
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610
      */
 
+    /*-------1.5.3-------*/
+    /*Deloppgave1
+    Krav 1 oppfylles fordi tilfelle(n) vil reduseres for hvert kall.
+    Krav 2 oppfylles fordi det har et basistilfelle som vil nås uten et till kall på metoden(n == 1).
+    Maksimal rekursjonsdybde er n siden det blir n lag på programstakken.
+     */
 
+    /*Deloppgave2
+    Krav 1 oppfykkes fordi tilfelle m reduseres hele tida, slik at avstanden mellom v og h blir mindre og mindre.
+    Krav 2 oppfylles fordi det har et basistilfelle(v == h) som vil nås tilslutt, og det ikke trengs flere kall.
+    Hvis metoden kalles med v = 0 og h = a.length-1, vil maksimal rekursjonsdybde bli ⌈log2(n)⌉ + 1 der n = a.length.
+     */
+
+    /*-------1.5.7-------*/
+
+    /*Deloppgave1
+    private static void kvikksortering0(int[] a, int v, int h)
+    {
+        System.out.println("Kallet med [" + v + ":" + h + "] starter!");
+        if (v >= h) return;   // tomt eller maks ett element
+
+        int k = sParter0(a,v,h,(v + h)/2);   // se Programkode 1.3.9 f)
+        if (v < k - 1) kvikksortering0(a, v, k - 1);    // sorterer intervallet a[v : p - 1]
+        if (k + 1 < h) kvikksortering0(a, k + 1, h);    // sorterer intervallet a[p + 1 : h]
+        System.out.println("Kallet med [" + v + ":" + h + "] er ferdig!");
+    }
+
+    public static void kvikksortering(int[] a) // sorterer hele tabellen
+    {
+        if (a.length > 1) kvikksortering0(a, 0, a.length - 1);
+    }
+     */
 
     public static void main(String[] args) {
 
