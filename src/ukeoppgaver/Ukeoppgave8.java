d9a659725afc0ad942899c95aedfa441835c399b
asmar <asmar-nasar2000@hotmail.com>
2020-10-12 11:30:55 +0200

    5.1.1 done

---
+++ src/ukeoppgaver/Ukeoppgave8.java
@@ -0,0 +1,49 @@
+package ukeoppgaver;
+
+public class Ukeoppgave8 {
+    /*-------5.1.1-------*/
+    /*Delopg1
+
+    a)Treet har 6 nivåer.
+    b)Noder på nivå 2: L O B
+    c)Noder på nivå 3: A E N G
+    d)Nivå 3 kan innholde 4 flere noder (dvs totalt 8 tilsammen)
+    e)Nivå 4 kan inneholde 13 noder til(totalt 16 tilsammen).
+    f)Etterkommere til A-noden: K F C
+    g)Forgjengere til A-noden: L I D
+    h)Noder som er besteforeldre: D H O I L A
+    i)Enebarn: L K
+    j)Treets høyde er 5.
+    k)Dyben til D-noden: 0
+    l)Høyden til I-nodens subtre: Venstresubtre: -1 og høyresubtre: 3
+    m)7 bladnoder
+    n)8 indre noder
+    o)
+     */
+
+    /*Delopg2
+    a)Treet har 5 nivåer.
+    b)Noder på nivå 2: G A H K
+    c)Noder på nivå 3: L O D N
+    d)Nivå 3 kan innholde 4 noder til.
+    e)Nivå 4 kan inneholde 12 noder til.
+    f)Etterkommere til A-noden: L O M C
+    g)Forgjengere til A-noden: I E
+    h)Noder som er besteforeldre: E I B A K
+    i)Enebarn: D N
+    j)Treets høyde er 4.
+    k)Dybden til D-noden: 3.
+    l)I-nodens venstresubtre: 0 og høyresubtre: 2
+    m)7 bladnoder
+    n)8 indre noder
+     */
+
+    /*Delopg3
+    a)Nodene med minst ett søskenbarn: L O B. En node kan ha maks 2 søskenbarn.
+    b)Nodenne med minst en tremening:A E N G. En node kan ha maks 4 tremeninger.
+     */
+
+
+
+
+}
