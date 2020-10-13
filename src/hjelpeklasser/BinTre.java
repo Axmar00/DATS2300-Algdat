6c719486340acdc7a22757117f7bf0bed6ebba98
asmar <asmar-nasar2000@hotmail.com>
2020-10-13 20:51:32 +0200

    5.1.5

--- src/hjelpeklasser/BinTre.java
+++ src/hjelpeklasser/BinTre.java
@@ -106,21 +106,34 @@ public class BinTre<T>           // et generisk binærtre
       return gammelverdi;
     }
 
-    public static void main(String[] args) {
-      int[] pos = {1,2,3,4,7,8,15,16,31,32,63,64,127,128,255};
-      Integer[] verdier = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-      BinTre<Integer> tre = new BinTre<>(pos,verdier);
+    public T fjern(int posisjon){
+      if(posisjon < 1) throw new IllegalArgumentException("Må ha posisjon > 0");
 
-      BinTre<Integer> tre2 = new BinTre<>();
-      int n = 2;
-      for (int i = 1; i <= 7; i++) {
-        tre2.leggInn(n,2*i);
-        tre2.leggInn(2*n-1,2*i+1);
-        n *= 2;
+      Node<T> p = rot,q = null;    // nodereferanser
+
+      int filter = Integer.highestOneBit(posisjon) >> 1;   // filter = 100...00
+
+      while (p != null && filter > 0)
+      {
+        q = p;
+        p = (posisjon & filter) == 0 ? p.venstre : p.høyre;
+        filter >>= 1;  // bitforskyver filter
       }
 
+      if(p == null) throw new IllegalArgumentException("Posisjonen " + posisjon + " er utafor treet");
 
+      if(p.venstre != null || p.høyre != null)
+        throw new IllegalArgumentException("Posisjonen " + posisjon + " tilhører en indre node");
 
+      if(p == rot) rot = null;
+      else if(p == q.høyre) q.høyre = null;
+      else q.venstre = null;
+
+      antall--;                       // fjerner en verdi fra treet
+      return p.verdi;
+    }
+
+    public static void main(String[] args) {
 
     }
 
