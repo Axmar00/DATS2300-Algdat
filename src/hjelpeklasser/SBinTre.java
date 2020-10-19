82067d8a4febb378a032fe005bceddd6785417a7
asmar <asmar-nasar2000@hotmail.com>
2020-10-19 12:12:36 +0200

    5.2.3, ikke ferdig

--- src/hjelpeklasser/SBinTre.java
+++ src/hjelpeklasser/SBinTre.java
@@ -39,6 +39,18 @@ public class SBinTre<T> // implements Beholder<T>
     return new SBinTre<>(c);
   }
 
+  public static <T> SBinTre<T> komparatorTre(T[] a, Comparator<? super T> c)
+  {
+    SBinTre<T> tre = new SBinTre<>(c);          // komparatoren c
+    for (T verdi : a) tre.leggInn(verdi);       // bygger opp treet
+    return tre;                                 // treet returneres
+  }
+
+  public static <T extends Comparable<? super T>> SBinTre<T> naturligOrdenTre(T[] a)
+  {
+    return komparatorTre(a, Comparator.naturalOrder());  // naturlig ordning
+  }
+
   public int antall()        // antall verdier i treet
   {
     return antall;
@@ -49,9 +61,73 @@ public class SBinTre<T> // implements Beholder<T>
     return antall == 0;
   }
 
+  public boolean leggInn(T verdi)    // skal ligge i class SBinTre
+  {
+    if (verdi == null) throw new NullPointerException("Ulovlig nullverdi!");
+
+    Node<T> p = rot, q = null;               // p starter i roten
+    int cmp = 0;                             // hjelpevariabel
+
+    while (p != null)       // fortsetter til p er ute av treet
+    {
+      q = p;                                 // q er forelder til p
+      cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
+      p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+    }
+
+    // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+    p = new Node<>(verdi);                   // oppretter en ny node
+
+    if (q == null) rot = p;                  // p blir rotnode
+    else if (cmp < 0) q.venstre = p;         // venstre barn til q
+    else q.høyre = p;                        // høyre barn til q
+
+    antall++;                                // én verdi mer i treet
+    return true;                             // vellykket innlegging
+  }
+
+  private static int høyde(Node<?> p)  // ? betyr vilkårlig type
+  {
+    if (p == null) return -1;          // et tomt tre har høyde -1
+
+    return 1 + Math.max(høyde(p.venstre), høyde(p.høyre));
+  }
+
+  public int høyde()
+  {
+    return høyde(rot);                 // kaller hjelpemetoden
+  }
+
   public void nullstill()
   {
     rot = null; antall = 0;
+  }
+
+  public String toString()                   // hører til SBinTre
+  {
+    StringBuilder s = new StringBuilder();   // StringBuilder
+    s.append('[');                           // starter med [
+    if (!tom()) toString(rot,s);             // den rekursive metoden
+    s.append(']');                           // avslutter med ]
+    return s.toString();                     // returnerer
+  }
+
+  private static <T> void toString(Node<T> p, StringBuilder s)
+  {
+    if (p.venstre != null)                   // p har et venstre subtre
+    {
+      toString(p.venstre, s);                // komma og mellomrom etter
+      s.append(',').append(' ');             // den siste i det venstre
+    }                                        // subtreet til p
+
+    s.append(p.verdi);                       // verdien i p
+
+    if (p.høyre != null)                     // p har et høyre subtre
+    {
+      s.append(',').append(' ');             // komma og mellomrom etter
+      toString(p.høyre, s);                  // p siden p ikke er den
+    }                                        // siste noden i inorden
   }
 
   public static void main(String[] args) {
