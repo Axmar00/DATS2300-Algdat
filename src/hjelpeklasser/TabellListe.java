970c4552ea41172ad50710104c36033a0bf1bc3f
asmar <asmar-nasar2000@hotmail.com>
2021-03-15 21:14:23 +0100

    Not sure what Im commiting, but it looks good

--- src/hjelpeklasser/TabellListe.java
+++ src/hjelpeklasser/TabellListe.java
@@ -5,8 +5,7 @@ import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class TabellListe<T> implements Liste<T>
-  {
+public class TabellListe<T> implements Liste<T> {
     private T[] a;
     private int antall;
     private int endringer;
@@ -14,13 +13,13 @@ public class TabellListe<T> implements Liste<T>
     @SuppressWarnings("unchecked")          // pga. konverteringen: Object[] -> T[]
     public TabellListe(int størrelse)       // konstruktør
     {
-      a = (T[])new Object[størrelse];       // oppretter tabellen
-      antall = 0;                           // foreløpig ingen verdier
+        a = (T[]) new Object[størrelse];       // oppretter tabellen
+        antall = 0;                           // foreløpig ingen verdier
     }
 
     public TabellListe()                    // standardkonstruktør
     {
-      this(10);                             // startstørrelse på 10
+        this(10);                             // startstørrelse på 10
     }
 
     /*public TabellListe(T[] b)                    // en T-tabell som parameter
@@ -34,243 +33,271 @@ public class TabellListe<T> implements Liste<T>
     }*/
     public TabellListe(T[] a)               // en T-tabell som parameter
     {
-      this.a = a.clone();                   // kloner parametertabellen
-      antall = a.length;                    // alle i tabellen
+        this.a = a.clone();                   // kloner parametertabellen
+        antall = a.length;                    // alle i tabellen
     }
 
-    public int antall()
-    {
-      return antall;          // returnerer antallet
+    public int antall() {
+        return antall;          // returnerer antallet
     }
 
-    public boolean tom()
-    {
-      return antall == 0;     // listen er tom hvis antall er 0
+    public boolean tom() {
+        return antall == 0;     // listen er tom hvis antall er 0
     }
 
     @Override
     public void nullstill() {
-      if(a.length > 10){
-        a = (T[])new Object[10];
-      }
-      else{
-        for(int i = 0; i < antall; i++){
-          a[i] = null;
+        if (a.length > 10) {
+            a = (T[]) new Object[10];
+        } else {
+            for (int i = 0; i < antall; i++) {
+                a[i] = null;
+            }
+            antall = 0;
         }
-        antall = 0;
-      }
-      endringer++;
+        endringer++;
     }
 
-    public T hent(int indeks)
-    {
-      indeksKontroll(indeks, false);   // false: indeks = antall er ulovlig
-      return a[indeks];                // returnerer er tabellelement
+    public T hent(int indeks) {
+        indeksKontroll(indeks, false);   // false: indeks = antall er ulovlig
+        return a[indeks];                // returnerer er tabellelement
     }
 
-    public int indeksTil(T verdi)
-    {
-      for (int i = 0; i < antall; i++)
-      {
-        if (a[i].equals(verdi)) return i;   // funnet!
-      }
-      return -1;   // ikke funnet!
+    public int indeksTil(T verdi) {
+        for (int i = 0; i < antall; i++) {
+            if (a[i].equals(verdi)) return i;   // funnet!
+        }
+        return -1;   // ikke funnet!
     }
 
     @Override
     public T oppdater(int indeks, T verdi) {
-      Objects.requireNonNull(verdi, "null er ulovlig!");
+        Objects.requireNonNull(verdi, "null er ulovlig!");
 
-      indeksKontroll(indeks, false);  // false: indeks = antall er ulovlig
+        indeksKontroll(indeks, false);  // false: indeks = antall er ulovlig
 
-      T gammelverdi = a[indeks];      // tar vare på den gamle verdien
-      a[indeks] = verdi;              // oppdaterer
-      endringer++;
-      return gammelverdi;
+        T gammelverdi = a[indeks];      // tar vare på den gamle verdien
+        a[indeks] = verdi;              // oppdaterer
+        endringer++;
+        return gammelverdi;
     }
 
     @Override
     public boolean fjern(T verdi) {
-      Objects.requireNonNull(verdi, "null er ulovlig!");
+        Objects.requireNonNull(verdi, "null er ulovlig!");
 
-      for(int i = 0; i < antall; i++){
-        if(a[i].equals(verdi)){
-          antall--;
-          System.arraycopy(a,i+1,a,i,antall-i);
-          a[antall] = null;
-          endringer++;
-          return true;
+        for (int i = 0; i < antall; i++) {
+            if (a[i].equals(verdi)) {
+                antall--;
+                System.arraycopy(a, i + 1, a, i, antall - i);
+                a[antall] = null;
+                endringer++;
+                return true;
+            }
         }
-      }
-      return false;
+        return false;
     }
 
     @Override
     public T fjern(int indeks) {
-      indeksKontroll(indeks, false);  // false: indeks = antall er ulovlig
-      T verdi = a[indeks];
+        indeksKontroll(indeks, false);  // false: indeks = antall er ulovlig
+        T verdi = a[indeks];
 
-      antall--; // sletter ved å flytte verdier mot venstre
-      System.arraycopy(a, indeks + 1, a, indeks, antall - indeks);
-      a[antall] = null;   // tilrettelegger for "søppeltømming"
+        antall--; // sletter ved å flytte verdier mot venstre
+        System.arraycopy(a, indeks + 1, a, indeks, antall - indeks);
+        a[antall] = null;   // tilrettelegger for "søppeltømming"
 
-      endringer++;
-      return verdi;
+        endringer++;
+        return verdi;
     }
 
     @Override
     public boolean leggInn(T verdi) {
-      Objects.requireNonNull(verdi, "null er ulovlig!");
+        Objects.requireNonNull(verdi, "null er ulovlig!");
 
-      // En full tabell utvides med 50%
-      if (antall == a.length)
-      {
-        a = Arrays.copyOf(a,(3*antall)/2 + 1);
-      }
+        // En full tabell utvides med 50%
+        if (antall == a.length) {
+            a = Arrays.copyOf(a, (3 * antall) / 2 + 1);
+        }
 
-      a[antall++] = verdi;   // setter inn ny verdi
+        a[antall++] = verdi;   // setter inn ny verdi
 
-      endringer++;
-      return true;
+        endringer++;
+        return true;
     }
 
     @Override
     public void leggInn(int indeks, T verdi) {
-      Objects.requireNonNull(verdi, "null er ulovlig!");
+        Objects.requireNonNull(verdi, "null er ulovlig!");
 
-      indeksKontroll(indeks, true);  // true: indeks = antall er lovlig
+        indeksKontroll(indeks, true);  // true: indeks = antall er lovlig
 
-      // En full tabell utvides med 50%
-      if (antall == a.length) a = Arrays.copyOf(a,(3*antall)/2 + 1);
+        // En full tabell utvides med 50%
+        if (antall == a.length) a = Arrays.copyOf(a, (3 * antall) / 2 + 1);
 
-      // rydder plass til den nye verdien
-      System.arraycopy(a, indeks, a, indeks + 1, antall - indeks);
+        // rydder plass til den nye verdien
+        System.arraycopy(a, indeks, a, indeks + 1, antall - indeks);
 
-      a[indeks] = verdi;     // setter inn ny verdi
+        a[indeks] = verdi;     // setter inn ny verdi
 
-      antall++;
-      endringer++;
+        antall++;
+        endringer++;
     }
 
-    public boolean inneholder(T verdi)
-    {
-      return indeksTil(verdi) != -1;
+    public boolean inneholder(T verdi) {
+        return indeksTil(verdi) != -1;
     }
 
     @Override
-    public String toString(){
-      if(antall == 0) return "[]";
+    public String toString() {
+        if (antall == 0) return "[]";
 
-      StringBuilder s = new StringBuilder();
-      s.append("[").append(a[0]);
+        StringBuilder s = new StringBuilder();
+        s.append("[").append(a[0]);
 
-      for(int i = 1; i < antall; i++){
-        s.append(",").append(" ").append(a[i]);
-      }
-      s.append("]");
-      return s.toString();
+        for (int i = 1; i < antall; i++) {
+            s.append(",").append(" ").append(a[i]);
+        }
+        s.append("]");
+        return s.toString();
     }
 
-    public void forEach(Consumer<? super T> action){
-      for(int i = 0; i < antall; i++){
-        action.accept(a[i]);
-      }
+    public void forEach(Consumer<? super T> action) {
+        for (int i = 0; i < antall; i++) {
+            action.accept(a[i]);
+        }
     }
 
     public boolean fjernHvis(Predicate<? super T> p)  // betingelsesfjerning
     {
-      Objects.requireNonNull(p);                       // kaster unntak
+        Objects.requireNonNull(p);                       // kaster unntak
 
-      int nyttAntall = antall;
+        int nyttAntall = antall;
 
-      for (int i = 0, j = 0; j < antall; j++)
-      {
-        if (p.test(a[j])) nyttAntall--;   // a[j] skal fjernes
-        else a[i++] = a[j];               // forskyver
-      }
+        for (int i = 0, j = 0; j < antall; j++) {
+            if (p.test(a[j])) nyttAntall--;   // a[j] skal fjernes
+            else a[i++] = a[j];               // forskyver
+        }
 
-      for (int i = nyttAntall; i < antall; i++)
-      {
-        a[i] = null;   // tilrettelegger for "søppeltømming"
-      }
+        for (int i = nyttAntall; i < antall; i++) {
+            a[i] = null;   // tilrettelegger for "søppeltømming"
+        }
 
-      boolean fjernet = nyttAntall < antall;
+        boolean fjernet = nyttAntall < antall;
 
-      if(fjernet) endringer++;
+        if (fjernet) endringer++;
 
-      antall = nyttAntall;
-      return fjernet;
+        antall = nyttAntall;
+        return fjernet;
     }
 
 
-  @Override
+    @Override
     public Iterator<T> iterator() {
-      return new TabellListeIterator();
+        return new TabellListeIterator();
     }
 
     //Indre klasse i class TabellListe
-    private class TabellListeIterator implements Iterator<T>
-    {
-      private int denne = 0;       // instansvariabel
-      private int iteratorendringer = endringer;
+    private class TabellListeIterator implements Iterator<T> {
+        private int denne = 0;       // instansvariabel
+        private int iteratorendringer = endringer;
 
-      public boolean hasNext()     // sjekker om det er flere igjen
-      {
-        return denne < antall;     // sjekker verdien til denne
-      }
-
-      private boolean fjernOK = false;   // ny instansvariabel i TabellListeIterator
-
-      public T next()                    // ny versjon
-      {
-        if (iteratorendringer != endringer)
+        public boolean hasNext()     // sjekker om det er flere igjen
         {
-          throw new ConcurrentModificationException("Listen er endret!");
+            return denne < antall;     // sjekker verdien til denne
         }
 
-        if (!hasNext())
-          throw new NoSuchElementException("Tomt eller ingen verdier igjen!");
+        private boolean fjernOK = false;   // ny instansvariabel i TabellListeIterator
 
-        T denneVerdi = a[denne];   // henter aktuell verdi
-        denne++;                   // flytter indeksen
-        fjernOK = true;            // nå kan remove() kalles
-
-        return denneVerdi;         // returnerer verdien
-      }
-
-      public void remove()         // ny versjon
-      {
-        if (iteratorendringer != endringer) throw new
-                ConcurrentModificationException("Listen er endret!");
-
-        if (!fjernOK) throw
-                new IllegalStateException("Ulovlig tilstand!");
-
-        fjernOK = false;           // remove() kan ikke kalles på nytt
-
-        // verdien i denne - 1 skal fjernes da den ble returnert i siste kall
-        // på next(), verdiene fra og med denne flyttes derfor en mot venstre
-        antall--;           // en verdi vil bli fjernet
-        denne--;            // denne må flyttes til venstre
-
-        System.arraycopy(a, denne + 1, a, denne, antall - denne);  // tetter igjen
-        a[antall] = null;   // verdien som lå lengst til høyre nulles
-
-        endringer++;
-        iteratorendringer++;
-      }
-
-      public void forEachRemaining(Consumer<? super T> action){
-        while (denne < antall)
+        public T next()                    // ny versjon
         {
-          action.accept(a[denne++]);
+            if (iteratorendringer != endringer) {
+                throw new ConcurrentModificationException("Listen er endret!");
+            }
+
+            if (!hasNext())
+                throw new NoSuchElementException("Tomt eller ingen verdier igjen!");
+
+            T denneVerdi = a[denne];   // henter aktuell verdi
+            denne++;                   // flytter indeksen
+            fjernOK = true;            // nå kan remove() kalles
+
+            return denneVerdi;         // returnerer verdien
         }
-      }
+
+        public void remove()         // ny versjon
+        {
+            if (iteratorendringer != endringer) throw new
+                    ConcurrentModificationException("Listen er endret!");
+
+            if (!fjernOK) throw
+                    new IllegalStateException("Ulovlig tilstand!");
+
+            fjernOK = false;           // remove() kan ikke kalles på nytt
+
+            // verdien i denne - 1 skal fjernes da den ble returnert i siste kall
+            // på next(), verdiene fra og med denne flyttes derfor en mot venstre
+            antall--;           // en verdi vil bli fjernet
+            denne--;            // denne må flyttes til venstre
+
+            System.arraycopy(a, denne + 1, a, denne, antall - denne);  // tetter igjen
+            a[antall] = null;   // verdien som lå lengst til høyre nulles
+
+            endringer++;
+            iteratorendringer++;
+        }
+
+        public void forEachRemaining(Consumer<? super T> action) {
+            while (denne < antall) {
+                action.accept(a[denne++]);
+            }
+        }
 
     }  // TabellListeIterator
 
-    public static void main(String[] args) {
-
+    public static int[] snitt(int[] a, int[] b) {
+        int[] array;
+        if (a.length > b.length) {
+            array = new int[b.length];
+        } else {
+            array = new int[a.length];
+        }
+        int antallLike = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    array[antallLike++] = a[i];
+                    break;
+                }
+            }
+        }
+        return array;
     }
 
-  }
+    public static void frekvensFordeling(int[] a) {
+        if (a.length == 0) System.out.println(0 + " " + 0);
+        int j = 0;
+        int tall = a[j];
+
+        while (j < a.length) {
+            int teller = 0;
+            for (int i = j; i < a.length; i++) {
+                if (tall == a[i]) {
+                    teller++;
+                }
+                if(tall < a[i]){ break;}
+            }
+            System.out.println(tall + " " + teller);
+            if (teller > 1){ j += teller;}
+            else{ j++;}
+
+            if (j < a.length){ tall = a[j];}
+            else{ break;}
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/oblig1/Oppgave1.java
+++ src/oblig1/Oppgave1.java
@@ -37,7 +37,6 @@ public class Oppgave1 {
         }
         return ombyttinger;
     }
-    public static void main(String[] args){
 
-    }
+
 }
